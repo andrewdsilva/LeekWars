65f43f2851f019df1c4701e556bc26b693a3abd1
Andrew <andrew.dasilva042@gmail.com>
2016-05-18 09:45:07 +0200

    Package node

--- init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/"
-
-echo "Installing modules in $path"
-
-npm install request                   --prefix $path
-npm install fs                        --prefix $path
-npm install fs-extra                  --prefix $path
