9d3c36e8b5c7cb998cbf072e6a75bbe1e98e9ff2
David Mills <dmills@devatics.com>
2016-07-08 12:04:05 +0200

    Adding auto fight for solo leeks

--- leekwars.js
+++ leekwars.js
@@ -11,9 +11,15 @@ var commands = require('./lib/commands');
 
 require('yargs')
     .usage('$0 <cmd> [args]')
-    .command('garden-solo <name>', 'Look for a solo garden fight for a leek', {}, function(argv){
-        commands.garden_start_solo_fight(argv);
-    })
+    .command('garden-solo <name>', 'Look for a solo garden fight for a leek', {
+                auto: {
+                alias: 'a',
+                describe: 'Automatically choose oponent',
+                }
+            },
+        function(argv){
+            commands.garden_start_solo_fight(argv);
+        })
     .command('garden-farmer', 'Look for a solo garden fight for a farmer', {}, function(argv){
         commands.garden_start_farmer_fight();
     })
